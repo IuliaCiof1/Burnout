84c63da71fb22ca69e179dc525d0ad2dd5c6523e
Odashi1 <150281861+Odashi1@users.noreply.github.com>
2024-10-31 21:12:08 +0200

    Update SWITCH_Interacting.cs

--- Assets/Scripts/Interactable Objects/SWITCH_Interacting.cs
+++ Assets/Scripts/Interactable Objects/SWITCH_Interacting.cs
@@ -5,7 +5,7 @@ public class SWITCH_Interacting : MonoBehaviour, IInteractable
 {
     private void Start()
     {
-        
+     // Astea o sa aiba o lista cu toate obiectele    
     }
     public void Interact()
     {
