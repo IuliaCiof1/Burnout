1a3aedd700ba06c05f7a0e6e5480884b1518f354
Odashi1 <150281861+Odashi1@users.noreply.github.com>
2024-11-06 21:51:18 +0200

    Refactoring ELEVATOR_interactable

--- Assets/Scripts/Interactable Objects/ELEVATOR_interactable.cs
+++ Assets/Scripts/Interactable Objects/ELEVATOR_interactable.cs
@@ -1,50 +1,48 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ELEVATOR_interactable : MonoBehaviour, IInteractable
 {
+    public Transform targetPosition;
+    public float elevatorSpeed = 1.0f;
+    public Animator doorAnimator;
+    private bool isMoving = false;
+    private GameObject player;
 
-    private GameObject Player;
-    private bool PlayerInside = false;
-
-    // Start is called before the first frame update
     void Start()
     {
-        Player = GameObject.Find("PlayerManager");
-    }
-
-    private void Update()
-    {
-        if(Vector3.Distance(Player.transform.position, this.transform.GetChild(0).GetChild(0).Find("Plane_16").transform.position) < 1f)
-        {
-            PlayerInside = true;
-        }
-        else
-        {
-            PlayerInside = false;
-        }
+        player = GameObject.FindWithTag("Player");
     }
 
     public void Interact()
     {
-        StartCoroutine(ElevatorCoroutine());
+        if (!isMoving)
+        {
+            StartCoroutine(OpenDoorsThenMove());
+        }
     }
 
-    IEnumerator ElevatorCoroutine()
+    private IEnumerator OpenDoorsThenMove()
     {
-        this.gameObject.GetComponent<Animator>().SetBool("ButtonPressed", true);
-        yield return new WaitForSecondsRealtime(8f);
-        gameObject.GetComponent<BoxCollider>().enabled = false;
-        this.gameObject.GetComponent<Animator>().SetFloat("AnimSpeed", 0f);
-        while(PlayerInside == false)
+        isMoving = true;
+        //check for player leaving
+        doorAnimator.SetBool("hasStarted",true);
+        yield return new WaitForSeconds(6f);
+        yield return StartCoroutine(MoveToTarget());
+        yield return new WaitForSeconds(2f);
+        doorAnimator.SetBool("hasArrived", true);
+        //check for player leaving
+        doorAnimator.SetBool("closeDoor", true);
+
+        isMoving = false;
+    }
+
+    private IEnumerator MoveToTarget()
+    {
+        while (Vector3.Distance(transform.position, targetPosition.position) > 0.01f)
         {
+            transform.position = Vector3.MoveTowards(transform.position, targetPosition.position, elevatorSpeed * Time.deltaTime);
             yield return null;
         }
-        Player.transform.parent = this.gameObject.transform;
-        this.gameObject.GetComponent<Animator>().SetFloat("AnimSpeed", 1f);
-        yield return new WaitForSecondsRealtime(4f);
-        this.transform.position += new Vector3(0, -3f, 0);
-        Player.transform.parent = GameObject.Find("==== Player ====").transform;
     }
 }
