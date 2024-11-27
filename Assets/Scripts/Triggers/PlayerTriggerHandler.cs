d94c87441e93ead710555fd35450668064459a42
IuliaCiof1 <iulia.ciofindac@gmail.com>
2024-11-27 20:08:38 +0200

    door magic fix

--- Assets/Scripts/Triggers/PlayerTriggerHandler.cs
+++ Assets/Scripts/Triggers/PlayerTriggerHandler.cs
@@ -16,7 +16,7 @@ public class PlayerTriggerHandler : MonoBehaviour
             //Destroy(other.gameObject);
             //triggerZones[i].enabled = false;
         }
-            if (triggerZones[0] != null)
+            if (triggerZones.Length!=0)
                 Destroy(other.gameObject);
 
     }
