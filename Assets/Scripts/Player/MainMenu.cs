47b65eb6c59dd6eeb291c73c9f046f19be96c7b7
Odashi1 <150281861+Odashi1@users.noreply.github.com>
2024-12-08 19:03:02 +0200

    Main Menu code

---
+++ Assets/Scripts/Player/MainMenu.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour
+{
+    public Image fadeImage;
+    public float fadeDuration = 1f; 
+
+    public void Play()
+    {
+        StartCoroutine(FadeOutAndLoadScene("Main"));
+    }
+
+    public void Quit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+
+    private IEnumerator FadeOutAndLoadScene(string sceneName)
+    {
+        fadeImage.gameObject.SetActive(true);
+        Color color = fadeImage.color;
+        color.a = 0;
+        fadeImage.color = color;
+
+        float timer = 0f;
+        while (timer < fadeDuration)
+        {
+            timer += Time.deltaTime;
+            color.a = Mathf.Clamp01(timer / fadeDuration);
+            fadeImage.color = color;
+            yield return null;
+        }
+
+        SceneManager.LoadScene(sceneName);
+    }
+}
